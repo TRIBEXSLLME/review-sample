e3261fd8013e19c92e0f34260e9db26c4927ca94
Khalid ibrahim <masterpiekhalid@gmail.com>
2024-05-09 21:17:01 +0100

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Review from './Reviews'
+import './App.css'
+
+function App() {
+  
+
+  return  <main>
+    <section className="container">
+      <div className="title">
+        <h2>our reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
+    </section>
+  </main>;
+     
+  
+  
+}
+
+export default App
